e093d0034612dd3b916f6328167b7d0bc1158096
LuisPulenta <luisalbertonu@gmail.com>
2023-05-19 16:01:10 -0300

    Video 404

---
+++ lib/features/products/presentation/providers/products_repository_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'package:teslo_shop/features/products/infrastructures/datasources/products_datasource_impl.dart';
+import 'package:teslo_shop/features/products/infrastructures/repositories/products_repository_impl.dart';
+
+final productsRepositoryProvider = Provider<ProductsRepository>((ref) {
+  final accessToken = ref.watch(authProvider).user?.token ?? '';
+  final productsRepository =
+      ProductsRepositoryImpl(ProductsDatasourceImpl(accessToken: accessToken));
+  return productsRepository;
+});
